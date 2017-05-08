fad66e24ec0f9d854322f015ab488ba2e32f9639
LeandroTorresCoutinho <leandro.torres.coutinho@gmail.com>
2017-05-08 10:32:43 -0300

    inserting google app secret and key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_KEY'], ENV['FACEBOOK_APP_SECRET']
+  provider :google_oauth2, ENV['GOOGLE_APP_KEY'], ENV['GOOGLE_APP_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}}
 end
