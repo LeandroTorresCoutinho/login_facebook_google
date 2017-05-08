0bddb74d661fac6a49d5e18d4de703b99ff4bdc5
LeandroTorresCoutinho <leandro.torres.coutinho@gmail.com>
2017-05-08 10:34:37 -0300

    including session and home routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 	match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 	match 'auth/failure', to: redirect('/'), via: [:get, :post]
 	match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
+	
+	resources :sessions, only: [:create, :destroy]
+	resource :home, only: [:show]
+
+	root to: "home#show"
 end
